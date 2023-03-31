1f0854051d0b62a96b31b359b2ff251cf2f94754
stefanoschoonheimMBO <320791@student.mboutrecht.nl>
2023-03-31 11:07:13 +0200

    config.php with connection to database

---
+++ app/config/config.php
@@ -0,0 +1,6 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'mvc-oop-toets-herk');
+    define('URLROOT', 'mvc-oop-toets-herk.com');
